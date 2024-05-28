5933b6d28dc97ce2f325dd23bfab0a35b8b18b4f
Mridul Verma <142104907+Muddyworm@users.noreply.github.com>
2024-05-28 12:11:05 +0000

    commit

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "mridul@google.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+accountEmail = "verma@google.com"
+accountPassword = "21212121"
+accountCity = "Goa"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3+3 ) , we are using node.js not browser
+/*
+String
+Big Int
+Number = 2 to the power 53
+Null = standalone value
+Undefined = not defined
+Boolean = true/false
+*/
